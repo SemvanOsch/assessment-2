4ff2b59d5d55c30846091900ec7ea38ea7f51bba
SemvanOsch <145602882+SemvanOsch@users.noreply.github.com>
2025-06-22 12:48:59 +0200

    Pages modification

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/assessment-2/', // <-- dit is belangrijk voor GitHub Pages
   plugins: [react()],
-})
+});
